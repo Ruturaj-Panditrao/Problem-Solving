08c7a4e360ea185ea249c2781b6ddfc6e5e91c2d
Ruturaj-Panditrao <94856890+Ruturaj-Panditrao@users.noreply.github.com>
2023-02-23 01:03:18 +0530

    Create 12BinaryTreePreorderTraversal.cpp

---
+++ 12BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,24 @@
+/*
+Problem Link :
+https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+*/
+
+class Solution
+{
+public:
+
+    void preOrderTraversal(TreeNode* root, vector<int>&ans)
+    {
+        if(root == NULL) return;
+        ans.push_back(root->val);
+        preOrderTraversal(root->left,ans);
+        preOrderTraversal(root->right,ans);
+        return;
+    }
+    vector<int> preorderTraversal(TreeNode* root) 
+    {
+        vector<int>ans;
+        preOrderTraversal(root,ans);
+        return ans;
+    }
+};
