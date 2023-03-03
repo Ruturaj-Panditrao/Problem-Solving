ab4cc14e8c639044a1faf50b8ebe20e110fddea5
Ruturaj-Panditrao <94856890+Ruturaj-Panditrao@users.noreply.github.com>
2023-03-04 01:11:54 +0530

    Create 50ConcatenationOfArray.cpp

---
+++ 50ConcatenationOfArray.cpp
@@ -0,0 +1,20 @@
+/*
+Problem Link :
+https://leetcode.com/problems/concatenation-of-array/description/
+*/
+
+class Solution
+{
+public:
+    vector<int> getConcatenation(vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<int>ans(2*n,0);
+        for(int i=0; i<n; i++)
+        {
+            ans[i] = nums[i];
+            ans[n+i] = nums[i];
+        }    
+        return ans;
+    }
+};
