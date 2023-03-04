31053b10c271fe397c29a74504a2cebb11d8fd3d
Ruturaj-Panditrao <94856890+Ruturaj-Panditrao@users.noreply.github.com>
2023-03-04 10:25:21 +0530

    Create 51SameTree.cpp

---
+++ 51SameTree.cpp
@@ -0,0 +1,19 @@
+/*
+Problem Link :
+https://leetcode.com/problems/same-tree/description/
+*/
+
+class Solution 
+{
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) 
+    {
+        if(p==NULL || q==NULL)
+        {
+            return (p==q);
+        }
+
+        return (p->val == q->val) && isSameTree(p->left,q->left)
+        && isSameTree(p->right,q->right);
+    }
+};
