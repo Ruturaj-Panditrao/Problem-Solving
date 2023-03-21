89d378aa43d6319f97e7c5e0bcfe1fea1151c3aa
Ruturaj-Panditrao <94856890+Ruturaj-Panditrao@users.noreply.github.com>
2023-03-21 23:55:22 +0530

    Create 90SearchInsertPosition.cpp

---
+++ 90SearchInsertPosition.cpp
@@ -0,0 +1,24 @@
+/*
+Problem Link :
+https://leetcode.com/problems/search-insert-position/description/?envType=study-plan&id=algorithm-i
+*/
+
+class Solution
+{
+public:
+    int searchInsert(vector<int>& nums, int target)
+    {
+        int n = nums.size();
+        int start = 0, end = n-1;
+        while(start<=end){
+            int mid = (start) + (end-start)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] > target)
+                end = mid-1;
+            else
+                start = mid+1;     
+        }
+        return start;
+    }
+};
