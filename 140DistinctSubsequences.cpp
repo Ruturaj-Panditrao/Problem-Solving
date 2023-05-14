70d61311393c952d294e5baf2222510c12739a84
Ruturaj-Panditrao <94856890+Ruturaj-Panditrao@users.noreply.github.com>
2023-05-14 23:46:35 +0530

    Create 140DistinctSubsequences.cpp

---
+++ 140DistinctSubsequences.cpp
@@ -0,0 +1,123 @@
+/*
+Problem Link : https://leetcode.com/problems/distinct-subsequences/
+*/
+
+// Recursive :
+
+class Solution 
+{
+public:
+    
+    int recurse(int i, int j, string s, string t)
+    {
+        if(j<0) return 1;
+        if(i<0) return 0;
+        if(s[i]==t[j])
+        {
+            return recurse(i-1,j-1,s,t) + recurse(i-1,j,s,t);
+        }
+        else
+        {
+            return recurse(i-1,j,s,t);
+        }
+
+    }
+    int numDistinct(string s, string t) 
+    {
+        int n = s.length();
+        int m = t.length();
+        return recurse(n-1,m-1,s,t);    
+    }
+};
+
+// Memoized
+
+class Solution 
+{
+public:
+    
+    int recurse(int i, int j, string s, string t, vector<vector<int>>&dp)
+    {
+        if(j<0) return 1;
+        if(i<0) return 0;
+        if(dp[i][j]!= -1) return dp[i][j];
+        if(s[i]==t[j])
+        {
+            return dp[i][j] = recurse(i-1,j-1,s,t,dp) + recurse(i-1,j,s,t,dp);
+        }
+        else
+        {
+            return dp[i][j] = recurse(i-1,j,s,t,dp);
+        }
+
+    }
+    int numDistinct(string s, string t) 
+    {
+        int n = s.length();
+        int m = t.length();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return recurse(n-1,m-1,s,t,dp);    
+    }
+};
+
+// Tabulated
+
+class Solution 
+{
+public:
+    int numDistinct(string s, string t) 
+    {
+        int n = s.length();
+        int m = t.length();
+        vector<vector<double>>dp(n+1,vector<double>(m+1,0));
+        for(int i=0; i<=n; i++) dp[i][0] = 1;
+        for(int i=1; i<=m; i++) dp[0][i] = 0;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return (int)dp[n][m];
+    }
+};
+
+// Space Optimized
+
+class Solution 
+{
+public:
+    int numDistinct(string s, string t) 
+    {
+        int n = s.length();
+        int m = t.length();
+        vector<double>prev(m+1,0);
+        vector<double>curr(m+1,0);
+        prev[0] = 1;
+        curr[0] = 1;
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    curr[j] = prev[j-1] + prev[j];
+                }
+                else
+                {
+                    curr[j] = prev[j];
+                }
+            }
+            prev = curr;
+        }
+        return (int)prev[m];
+    }
+};
